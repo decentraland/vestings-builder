6d8481adea0a39ff9abf32f59a2e505461c46458
Nando <24811313+fzavalia@users.noreply.github.com>
2022-08-20 17:54:02 -0300

    chore: Add test suites

--- test/VestingImpl.spec.js
+++ test/VestingImpl.spec.js
@@ -18,20 +18,20 @@ describe("TokenVesting", function () {
   let beneficiary;
   let testToken;
   let tokenVesting;
-  let initializeParams;
+  let initParams;
 
   function initializeTokenVesting() {
     return tokenVesting
       .connect(deployer)
       .initialize(
-        initializeParams.owner,
-        initializeParams.beneficiary,
-        initializeParams.start,
-        initializeParams.cliff,
-        initializeParams.duration,
-        initializeParams.revokable,
-        initializeParams.token,
-        initializeParams.period
+        initParams.owner,
+        initParams.beneficiary,
+        initParams.start,
+        initParams.cliff,
+        initParams.duration,
+        initParams.revokable,
+        initParams.token,
+        initParams.period
       );
   }
 
@@ -54,7 +54,7 @@ describe("TokenVesting", function () {
 
     const start = await getPendingBlockTimestamp();
 
-    initializeParams = {
+    initParams = {
       owner: owner.address,
       beneficiary: beneficiary.address,
       start,
@@ -85,28 +85,72 @@ describe("TokenVesting", function () {
 
     it("should return 250000 when the cliff is reached", async function () {
       expect(await getReleasableAmount()).to.be.equal(0);
-      await increaseTime(initializeParams.cliff);
+      await increaseTime(initParams.cliff);
       expect(await getReleasableAmount()).to.be.equal(250000);
     });
 
     it("should return 1000000 when the duration timestamp is reached", async function () {
       expect(await getReleasableAmount()).to.be.equal(0);
-      await increaseTime(initializeParams.duration);
+      await increaseTime(initParams.duration);
       expect(await getReleasableAmount()).to.be.equal(1000000);
     });
 
-    it("should return 250000 when no periods have passed after the cliff", async function () {
-      expect(await getReleasableAmount()).to.be.equal(0);
-      await increaseTime(initializeParams.cliff + initializeParams.period / 2);
-      expect(await getReleasableAmount()).to.be.equal(250000);
-    });
+    describe("releasable amount by periods passed after cliff test suites", function () {
+      function test(amount, periods) {
+        it(`should return ${amount} when ${periods} periods have passed after the cliff`, async function () {
+          expect(await getReleasableAmount()).to.be.equal(0);
+          await increaseTime(initParams.cliff + initParams.period * periods);
+          expect(await getReleasableAmount()).to.be.equal(amount);
+        });
+      }
 
-    it("should return 250000 when 1 period has passed after the cliff", async function () {
-      expect(await getReleasableAmount()).to.be.equal(0);
-      await increaseTime(
-        initializeParams.cliff + initializeParams.period * 1.5
-      );
-      expect(await getReleasableAmount()).to.be.equal(375000);
+      describe("when no periods have passed, only the amount of tokens corresponding to the time up to the cliff are releasable.", function () {
+        test(250000, 0);
+        test(250000, 0.3);
+        test(250000, 0.6);
+        test(250000, 0.9);
+      });
+
+      describe("after periods are elapsed, the user is able to release an extra amount corresponding to relative time of the previous period in relation to the duration of the whole vesting duration.", function () {
+        test(375000, 1);
+        test(375000, 1.3);
+        test(375000, 1.6);
+        test(375000, 1.9);
+
+        test(500000, 2);
+        test(500000, 2.3);
+        test(500000, 2.6);
+        test(500000, 2.9);
+
+        test(625000, 3);
+        test(625000, 3.3);
+        test(625000, 3.6);
+        test(625000, 3.9);
+
+        test(750000, 4);
+        test(750000, 4.3);
+        test(750000, 4.6);
+        test(750000, 4.9);
+
+        test(875000, 5);
+        test(875000, 5.3);
+        test(875000, 5.6);
+        test(875000, 5.9);
+      });
+
+      describe("there are 6 possible periods after the cliff given the initialization params. That is why after 6 periods, all the tokens are releasable.", function () {
+        test(1000000, 6);
+        test(1000000, 6.3);
+        test(1000000, 6.6);
+        test(1000000, 6.9);
+      });
+
+      describe("more periods than possible in the duration of the vesting will just return all the tokens available for release.", function () {
+        test(1000000, 7);
+        test(1000000, 7.3);
+        test(1000000, 7.6);
+        test(1000000, 7.9);
+      });
     });
   });
 });
