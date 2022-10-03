1215561cf88afe47a4a5b3fa0e40569d9394b1e5
Nando <24811313+fzavalia@users.noreply.github.com>
2022-10-03 14:38:04 -0300

    chore: Add tests for setBeneficiary

--- test/PeriodicTokenVesting.spec.js
+++ test/PeriodicTokenVesting.spec.js
@@ -3,50 +3,51 @@ const { ethers } = require("hardhat");
 const helpers = require("@nomicfoundation/hardhat-network-helpers");
 
 describe("PeriodicTokenVesting", () => {
+  // Signers
+  let deployer;
+  let owner;
+  let beneficiary;
+  let treasury;
+  let extra;
+
+  // Contracts
+  let vestingImpl;
+  let vesting;
+  let token;
+
+  // Params
+  let initParams;
+  let initParamsList;
+
+  beforeEach(async () => {
+    [deployer, owner, beneficiary, treasury, extra] = await ethers.getSigners();
+
+    const Token = await ethers.getContractFactory("MockToken");
+    token = await Token.deploy(ethers.utils.parseEther("100000"), treasury.address);
+
+    const PeriodicTokenVesting = await ethers.getContractFactory("PeriodicTokenVesting");
+    vestingImpl = await PeriodicTokenVesting.deploy();
+    await vestingImpl.deployed();
+
+    const Proxy = await ethers.getContractFactory("MockProxy");
+    const proxy = await Proxy.deploy(vestingImpl.address);
+
+    vesting = PeriodicTokenVesting.attach(proxy.address);
+
+    initParams = {
+      owner: owner.address,
+      beneficiary: beneficiary.address,
+      token: token.address,
+      revocable: true,
+      start: await helpers.time.latest(),
+      periodDuration: 7889400,
+      vestedPerPeriod: [0, 0, 0, 2500, 625, 625, 625, 625, 625, 625, 625, 625, 625, 625, 625, 625],
+    };
+
+    initParamsList = Object.values(initParams);
+  });
+
   describe("initialize", () => {
-    // Signers
-    let deployer;
-    let owner;
-    let beneficiary;
-    let treasury;
-
-    // Contracts
-    let vestingImpl;
-    let vesting;
-    let token;
-
-    // Params
-    let initParams;
-    let initParamsList;
-
-    beforeEach(async () => {
-      [deployer, owner, beneficiary, treasury] = await ethers.getSigners();
-
-      const Token = await ethers.getContractFactory("MockToken");
-      token = await Token.deploy(ethers.utils.parseEther("100000"), treasury.address);
-
-      const PeriodicTokenVesting = await ethers.getContractFactory("PeriodicTokenVesting");
-      vestingImpl = await PeriodicTokenVesting.deploy();
-      await vestingImpl.deployed();
-
-      const Proxy = await ethers.getContractFactory("MockProxy");
-      const proxy = await Proxy.deploy(vestingImpl.address);
-
-      vesting = PeriodicTokenVesting.attach(proxy.address);
-
-      initParams = {
-        owner: owner.address,
-        beneficiary: beneficiary.address,
-        token: token.address,
-        revocable: true,
-        start: await helpers.time.latest(),
-        periodDuration: 7889400,
-        vestedPerPeriod: [0, 0, 0, 2500, 625, 625, 625, 625, 625, 625, 625, 625, 625, 625, 625, 625],
-      };
-
-      initParamsList = Object.values(initParams);
-    });
-
     it("should have uninitialized values before initialization", async () => {
       expect(await vesting.owner()).to.equal(ethers.constants.AddressZero);
       expect(await vesting.getBeneficiary()).to.equal(ethers.constants.AddressZero);
@@ -97,7 +98,7 @@ describe("PeriodicTokenVesting", () => {
       initParamsList = Object.values(initParams);
 
       await expect(vesting.initialize(...initParamsList)).to.be.revertedWith(
-        "PeriodicTokenVesting#initialize: INVALID_BENEFICIARY"
+        "PeriodicTokenVesting#_setBeneficiary: INVALID_BENEFICIARY"
       );
     });
 
@@ -107,6 +108,38 @@ describe("PeriodicTokenVesting", () => {
 
       await expect(vesting.initialize(...initParamsList)).to.be.revertedWith(
         "PeriodicTokenVesting#initialize: INVALID_TOKEN"
+      );
+    });
+  });
+
+  describe("setBeneficiary", () => {
+    beforeEach(async () => {
+      await vesting.initialize(...initParamsList);
+    });
+
+    it("should change the beneficiary", async () => {
+      expect(await vesting.getBeneficiary()).to.equal(beneficiary.address);
+
+      await vesting.connect(beneficiary).setBeneficiary(extra.address);
+
+      expect(await vesting.getBeneficiary()).to.equal(extra.address);
+    });
+
+    it("should emit a BeneficiaryUpdated event", async () => {
+      await expect(vesting.connect(beneficiary).setBeneficiary(extra.address))
+        .to.emit(vesting, "BeneficiaryUpdated")
+        .withArgs(extra.address, beneficiary.address);
+    });
+
+    it("reverts when sender is not the current beneficiary", async () => {
+      await expect(vesting.connect(extra).setBeneficiary(extra.address)).to.be.revertedWith(
+        "PeriodicTokenVesting#onlyBeneficiary: NOT_BENEFICIARY"
+      );
+    });
+
+    it("reverts when beneficiary is 0x0", async () => {
+      await expect(vesting.connect(beneficiary).setBeneficiary(ethers.constants.AddressZero)).to.be.revertedWith(
+        "PeriodicTokenVesting#_setBeneficiary: INVALID_BENEFICIARY"
       );
     });
   });
