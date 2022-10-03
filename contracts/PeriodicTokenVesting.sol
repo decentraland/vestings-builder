a64b6a4b39802a6ef014917fdc38ed7f5149378b
Nando <24811313+fzavalia@users.noreply.github.com>
2022-10-03 13:54:31 -0300

    feat: Add init variables and getters

--- contracts/PeriodicTokenVesting.sol
+++ contracts/PeriodicTokenVesting.sol
@@ -3,13 +3,60 @@
 pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts/interfaces/IERC20.sol";
 
 contract PeriodicTokenVesting is OwnableUpgradeable {
+    address private beneficiary;
+    IERC20 private token;
+    bool private isRevocable;
+    uint256 private start;
+    uint256 private periodDuration;
+    uint256[] private vestedPerPeriod;
+
     constructor() {
         _disableInitializers();
     }
 
-    function initialize() external initializer {
+    function initialize(
+        address _owner,
+        address _beneficiary,
+        IERC20 _token,
+        bool _isRevocable,
+        uint256 _start,
+        uint256 _periodDuration,
+        uint256[] calldata _vestedPerPeriod
+    ) external initializer {
         __Ownable_init();
+        transferOwnership(_owner);
+        beneficiary = _beneficiary;
+        token = _token;
+        isRevocable = _isRevocable;
+        start = _start;
+        periodDuration = _periodDuration;
+        vestedPerPeriod = _vestedPerPeriod;
+    }
+
+    function getBeneficiary() external view returns (address) {
+        return beneficiary;
+    }
+
+    function getToken() external view returns (IERC20) {
+        return token;
+    }
+
+    function getIsRevocable() external view returns (bool) {
+        return isRevocable;
+    }
+
+    function getStart() external view returns (uint256) {
+        return start;
+    }
+
+    function getPeriodDuration() external view returns (uint256) {
+        return periodDuration;
+    }
+
+    function getVestedPerPeriod() external view returns (uint256[] memory) {
+        return vestedPerPeriod;
     }
 }
