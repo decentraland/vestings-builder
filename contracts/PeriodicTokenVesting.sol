88f66c98b79a9a2b38e6382982aeeb545080d9d4
nando <24811313+fzavalia@users.noreply.github.com>
2022-10-03 14:17:31 -0300

    feat: Add parameters to initialize function (#33)
    * chore: Install hardhat network helpers
    
    * feat: Add MockToken
    
    * feat: Add init variables and getters
    
    * feat: Add more initialize tests
    
    * feat: Add validations and comments
    
    * chore: Add more initialize tests

--- contracts/PeriodicTokenVesting.sol
+++ contracts/PeriodicTokenVesting.sol
@@ -3,13 +3,90 @@
 pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts/interfaces/IERC20.sol";
 
 contract PeriodicTokenVesting is OwnableUpgradeable {
+    address private beneficiary;
+    IERC20 private token;
+    bool private isRevocable;
+    uint256 private start;
+    uint256 private periodDuration;
+    uint256[] private vestedPerPeriod;
+
     constructor() {
         _disableInitializers();
     }
 
-    function initialize() external initializer {
+    /// @notice Initialize the vesting contract.
+    /// @dev This function can only be called once.
+    /// @param _owner The Owner of the contract.
+    /// @param _beneficiary The beneficiary of the vested tokens.
+    /// @param _token The token to vest.
+    /// @param _isRevocable Whether the vesting contract is revocable.
+    /// @param _start The start time of the vesting.
+    /// @param _periodDuration The duration of each period.
+    /// @param _vestedPerPeriod The amount of tokens vested per period.
+    function initialize(
+        address _owner,
+        address _beneficiary,
+        address _token,
+        bool _isRevocable,
+        uint256 _start,
+        uint256 _periodDuration,
+        uint256[] calldata _vestedPerPeriod
+    ) external initializer {
+        require(
+            _beneficiary != address(0),
+            "PeriodicTokenVesting#initialize: INVALID_BENEFICIARY"
+        );
+
+        require(
+            _token != address(0),
+            "PeriodicTokenVesting#initialize: INVALID_TOKEN"
+        );
+
+        // Initialize Ownable
         __Ownable_init();
+
+        // Set the new owner (Checks that _owner != address(0) internally)
+        transferOwnership(_owner);
+
+        // Set the rest of the variables
+        beneficiary = _beneficiary;
+        token = IERC20(_token);
+        isRevocable = _isRevocable;
+        start = _start;
+        periodDuration = _periodDuration;
+        vestedPerPeriod = _vestedPerPeriod;
+    }
+
+    /// @notice Get the beneficiary of the vested tokens.
+    function getBeneficiary() external view returns (address) {
+        return beneficiary;
+    }
+
+    /// @notice Get the token to vest.
+    function getToken() external view returns (IERC20) {
+        return token;
+    }
+
+    /// @notice Get whether the vesting contract is revocable.
+    function getIsRevocable() external view returns (bool) {
+        return isRevocable;
+    }
+
+    /// @notice Get the start time of the vesting.
+    function getStart() external view returns (uint256) {
+        return start;
+    }
+
+    /// @notice Get the duration of each period.
+    function getPeriodDuration() external view returns (uint256) {
+        return periodDuration;
+    }
+
+    /// @notice Get the amount of tokens vested per period.
+    function getVestedPerPeriod() external view returns (uint256[] memory) {
+        return vestedPerPeriod;
     }
 }
--- test/PeriodicTokenVesting.spec.js
+++ test/PeriodicTokenVesting.spec.js
@@ -1,12 +1,30 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
+const helpers = require("@nomicfoundation/hardhat-network-helpers");
 
 describe("PeriodicTokenVesting", () => {
   describe("initialize", () => {
+    // Signers
+    let deployer;
+    let owner;
+    let beneficiary;
+    let treasury;
+
+    // Contracts
     let vestingImpl;
     let vesting;
+    let token;
+
+    // Params
+    let initParams;
+    let initParamsList;
 
     beforeEach(async () => {
+      [deployer, owner, beneficiary, treasury] = await ethers.getSigners();
+
+      const Token = await ethers.getContractFactory("MockToken");
+      token = await Token.deploy(ethers.utils.parseEther("100000"), treasury.address);
+
       const PeriodicTokenVesting = await ethers.getContractFactory("PeriodicTokenVesting");
       vestingImpl = await PeriodicTokenVesting.deploy();
       await vestingImpl.deployed();
@@ -15,25 +33,81 @@ describe("PeriodicTokenVesting", () => {
       const proxy = await Proxy.deploy(vestingImpl.address);
 
       vesting = PeriodicTokenVesting.attach(proxy.address);
+
+      initParams = {
+        owner: owner.address,
+        beneficiary: beneficiary.address,
+        token: token.address,
+        revocable: true,
+        start: await helpers.time.latest(),
+        periodDuration: 7889400,
+        vestedPerPeriod: [0, 0, 0, 2500, 625, 625, 625, 625, 625, 625, 625, 625, 625, 625, 625, 625],
+      };
+
+      initParamsList = Object.values(initParams);
     });
 
-    it("should have address(0) as owner before initialize", async () => {
+    it("should have uninitialized values before initialization", async () => {
       expect(await vesting.owner()).to.equal(ethers.constants.AddressZero);
+      expect(await vesting.getBeneficiary()).to.equal(ethers.constants.AddressZero);
+      expect(await vesting.getToken()).to.equal(ethers.constants.AddressZero);
+      expect(await vesting.getIsRevocable()).to.be.false;
+      expect(await vesting.getStart()).to.equal(ethers.constants.AddressZero);
+      expect(await vesting.getPeriodDuration()).to.equal(ethers.constants.AddressZero);
+      expect(await vesting.getVestedPerPeriod()).to.be.empty;
     });
 
-    it("should set the owner as the sender", async () => {
-      const [deployer] = await ethers.getSigners();
-      await vesting.initialize();
-      expect(await vesting.owner()).to.equal(deployer.address);
+    it("should initialize values", async () => {
+      await vesting.initialize(...initParamsList);
+
+      expect(await vesting.owner()).to.equal(initParams.owner);
+      expect(await vesting.getBeneficiary()).to.equal(initParams.beneficiary);
+      expect(await vesting.getToken()).to.equal(initParams.token);
+      expect(await vesting.getIsRevocable()).to.equal(initParams.revocable);
+      expect(await vesting.getStart()).to.equal(initParams.start);
+      expect(await vesting.getPeriodDuration()).to.equal(initParams.periodDuration);
+      expect((await vesting.getVestedPerPeriod()).map((x) => Number(x))).to.have.same.members(
+        initParams.vestedPerPeriod
+      );
     });
 
     it("reverts when initializing twice", async () => {
-      await vesting.initialize();
-      await expect(vesting.initialize()).to.be.revertedWith("Initializable: contract is already initialized");
+      await vesting.initialize(...initParamsList);
+
+      await expect(vesting.initialize(...initParamsList)).to.be.revertedWith(
+        "Initializable: contract is already initialized"
+      );
     });
 
     it("reverts when initializing the implementation", async () => {
-      await expect(vestingImpl.initialize()).to.be.revertedWith("Initializable: contract is already initialized");
+      await expect(vestingImpl.initialize(...initParamsList)).to.be.revertedWith(
+        "Initializable: contract is already initialized"
+      );
+    });
+
+    it("reverts when owner is 0x0", async () => {
+      initParams.owner = ethers.constants.AddressZero;
+      initParamsList = Object.values(initParams);
+
+      await expect(vesting.initialize(...initParamsList)).to.be.revertedWith("Ownable: new owner is the zero address");
+    });
+
+    it("reverts when beneficiary is 0x0", async () => {
+      initParams.beneficiary = ethers.constants.AddressZero;
+      initParamsList = Object.values(initParams);
+
+      await expect(vesting.initialize(...initParamsList)).to.be.revertedWith(
+        "PeriodicTokenVesting#initialize: INVALID_BENEFICIARY"
+      );
+    });
+
+    it("reverts when token is 0x0", async () => {
+      initParams.token = ethers.constants.AddressZero;
+      initParamsList = Object.values(initParams);
+
+      await expect(vesting.initialize(...initParamsList)).to.be.revertedWith(
+        "PeriodicTokenVesting#initialize: INVALID_TOKEN"
+      );
     });
   });
 });
