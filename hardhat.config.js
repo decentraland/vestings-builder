34a70950160e186d7e942bfa0f013dd9ba352d14
Nando <24811313+fzavalia@users.noreply.github.com>
2022-10-07 15:02:54 -0300

    chore: Update hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,26 @@
+const dotenv = require("dotenv");
+
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-etherscan");
+
+dotenv.config();
+
+const privateKey = process.env.PRIVATE_KEY;
+const rpc = process.env.RPC;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  networks: {
+    deploy: {
+      url: rpc || "",
+      accounts: privateKey ? [privateKey] : undefined,
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
   gasReporter: {
     enabled: true,
   },
+  solidity: "0.8.17",
 };
