015a2cf5ea2decc0db700657fa7c1dd4629006a2
Nando <24811313+fzavalia@users.noreply.github.com>
2022-11-15 15:24:22 -0300

    feat: Add react router dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,42 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-import CreateInBatch from './CreateInBatch'
-import { Web3ReactProvider } from '@web3-react/core'
-import { Web3Provider } from '@ethersproject/providers'
+import React from "react";
+import ReactDOM from "react-dom";
+import { createBrowserRouter, RouterProvider, redirect } from "react-router-dom";
+import { Web3ReactProvider } from "@web3-react/core";
+import { Web3Provider } from "@ethersproject/providers";
+import App from "./App";
+import CreateInBatch from "./CreateInBatch";
+import "./index.css";
 
 function getLibrary(provider: any) {
-  return new Web3Provider(provider)
+  return new Web3Provider(provider);
 }
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/batch",
+    element: <CreateInBatch />,
+  },
+  {
+    path: "/v2",
+    element: <div>/v2</div>,
+  },
+  {
+    path: "/v2/batch",
+    element: <div>/v2/batch</div>,
+  },
+  {
+    path: "*",
+    loader: () => redirect("/"),
+  },
+]);
 
 ReactDOM.render(
   <Web3ReactProvider getLibrary={getLibrary}>
-    {window.location.pathname === '/batch' ? <CreateInBatch /> : <App />}
+    <RouterProvider router={router} />
   </Web3ReactProvider>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
